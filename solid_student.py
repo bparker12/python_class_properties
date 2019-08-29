eff44d78cb1e61a493f0fee21a5af3c19fc3c0d5
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-08-29 14:11:29 -0500

    got it to work!

--- solid_student.py
+++ solid_student.py
@@ -1,66 +1,69 @@
 class Student:
 
-    def __init__(self):
+    # def __init__(self):
             # self.__first_name = ""
             # self.__last_name = ""
             # self.__age = 1
             # self.__cohort_num = 1
             # self.__full_name = self.__first_name + " " + self.__last_name
 
-            @property
-            def full_name(self):
-                return self.__full_name
+    @property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
 
-            @property
-            def first_name(self):
-                try:
-                    return self.__first_name
-                except AttributeError:
-                    return 0
+    @property
+    def first_name(self):
+        try:
+            return self.__first_name
+        except AttributeError:
+            return 0
 
-            @first_name.setter
-            def first_name(self, first_name):
-                if type(first_name) is str:
-                    self.__first_name = first_name
-                else:
-                    raise TypeError('Please provide a word for the first name')
+    @first_name.setter
+    def first_name(self, first_name):
+        if type(first_name) is str:
+            self.__first_name = first_name
+        else:
+            raise TypeError('Please provide a word for the first name')
 
-            @property
-            def last_name(self):
-                return self.__last_name
+    @property
+    def last_name(self):
+        return self.__last_name
 
-            @last_name.setter
-            def last_name(self, last_name):
-                if type(last_name) is str:
-                    self.__last_name = last_name
-                else:
-                    raise TypeError('Please provide a word for the last name')
+    @last_name.setter
+    def last_name(self, last_name):
+        if type(last_name) is str:
+            self.__last_name = last_name
+        else:
+            raise TypeError('Please provide a word for the last name')
 
-            @property
-            def age(self):
-                return self.__age
+    @property
+    def age(self):
+        return self.__age
 
-            @age.setter
-            def age(self, age):
-                if type(age) is int:
-                    self.__age = age
-                else:
-                    raise TypeError('Please provide a number for the age')
+    @age.setter
+    def age(self, age):
+        if type(age) is int:
+            self.__age = age
+        else:
+            raise TypeError('Please provide a number for the age')
 
-            @property
-            def cohort_num(self):
-                return self.__cohort_num
+    @property
+    def cohort_num(self):
+        return self.__cohort_num
 
-            @cohort_num.setter
-            def cohort_num(self, cohort_num):
-                if type(cohort_num) is int:
-                    self.__cohort_num = cohort_num
-                else:
-                    raise TypeError('Please provide a number for the cohort number')
+    @cohort_num.setter
+    def cohort_num(self, cohort_num):
+        if type(cohort_num) is int:
+            self.__cohort_num = cohort_num
+        else:
+            raise TypeError('Please provide a number for the cohort number')
 
 pam = Student()
-pam.first_name = 3
+pam.first_name = "Pam"
 pam.last_name = "Beasley"
 pam.age = 18
+pam.cohort_num = 33
+
+print(pam.full_name)
 
 print()
